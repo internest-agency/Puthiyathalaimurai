6ec458ee0f56436e9628e3f459c8ff53985c7a2e
Internest Agency <mail@internest.agency>
2023-08-01 11:05:04 +0530

    Timer Added

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,5 +1,21 @@
-const eventDate = new Date("08/11/2023 18:00:00");
+const days = document.querySelector('.day .timer span');
+const hours = document.querySelector('.hour .timer span');
+const mins = document.querySelector('.min .timer span');
+const secs = document.querySelector('.sec .timer span');
+
+setInterval(()=> {
+  const eventDate = new Date("08/11/2023 18:00:00");
 const currentDate = Date.now();
 const secRemains = Math.round((eventDate - currentDate)/1000);
+const secondsRemains = Math.round(secRemains%60);
+const minRemains = Math.round((secRemains%(3600))/60);
+const hoursRemains = Math.round((secRemains%(3600*24))/3600);
+const daysRemains = Math.round(secRemains/(3600*24));
 
-console.log(secRemains);
+days.innerText = daysRemains;
+hours.innerText = hoursRemains;
+mins.innerText = minRemains;
+secs.innerText = secondsRemains;
+
+
+}, 1000);
